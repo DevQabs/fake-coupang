2691ec00f6506544250949d2f1db8ac74b219efa
kimtaeguk <devqabs@gmail.com>
2022-02-27 22:19:40 +0900

    no message

--- src/main/java/com/coupang/www/product/application/ProductController.java
+++ src/main/java/com/coupang/www/product/application/ProductController.java
@@ -1,4 +1,21 @@
 package com.coupang.www.product.application;
 
+import org.springframework.hateoas.MediaTypes;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value = "/product", produces = MediaTypes.HAL_JSON_VALUE)
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class ProductController {
+
+    // 생성
+
+    // 승인요청
+
+    // 조회
+
+    // 삭제
+
+    // 
+
 }
